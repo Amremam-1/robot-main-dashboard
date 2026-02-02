7035f5de88ab2698007ee5efc8959f68a0637fd9
Amr Imam <emama2337@gmail.com>
2026-02-02 21:07:28 +0200

    updated btn actions

--- src/components/ActionsBtn.tsx
+++ src/components/ActionsBtn.tsx
@@ -1,10 +1,10 @@
 const ActionsBtn = () => {
   return (
-    <div className="flex items-center gap-4 justify-between mt-8 text-color">
-      <button className="w-1/3 bg-[#fe6c00] px-3 py-1.5 rounded-sm cursor-pointer">
+    <div className="flex items-center gap-4 justify-between md:flex-row flex-col mt-8 text-color">
+      <button className="w-full md:w-1/3 bg-[#fe6c00] px-3 py-1.5 rounded-sm cursor-pointer">
         Scan QR Code
       </button>
-      <button className="flex-1 border-color py-1.5 px-3 rounded-sm cursor-pointer">
+      <button className="w-full flex-1 border-color py-1.5 px-3 rounded-sm cursor-pointer">
         Delete Account
       </button>
     </div>
